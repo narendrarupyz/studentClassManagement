209f1aa661ac462720b43342ec9f2a7c746391ca
Narendra Kumar <narendra.kumar@rupyz.com>
2024-04-19 19:04:11 +0530

    Updated

--- student/serializers.py
+++ student/serializers.py
@@ -1,7 +1,8 @@
 from student.models import Student
 from rest_framework import serializers
 
+
 class StudentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Student
-        fields = ['name','roll_no','age','address','pincode','mobile_no']
+        fields = ['name', 'roll_no', 'age', 'address', 'pincode', 'mobile_no']
