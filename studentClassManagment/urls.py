3103e49b8a159c61e2202080c5fd7a21c0ed53b0
Narendra Kumar <narendra.kumar@rupyz.com>
2024-04-19 19:05:24 +0530

    Updated

--- studentClassManagment/urls.py
+++ studentClassManagment/urls.py
@@ -20,6 +20,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('student/',include('student.urls')),
-    path('classRooms',include('classRooms.urls')),
+    path('',include('student.urls')),
+    path('',include('classRooms.urls')),
 ]
