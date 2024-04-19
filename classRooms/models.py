04a75070a7ac461bea5551638ae68977f100eb00
Narendra Kumar <narendra.kumar@rupyz.com>
2024-04-19 18:57:22 +0530

    Created, model added

--- classRooms/models.py
+++ classRooms/models.py
@@ -1,16 +1,18 @@
 from django.db import models
 
-class ClassRoom:
+
+class ClassRoom(models.Model):
     AC = 'A/C'
     NON_AC = 'NON A/C'
     CLASSROOM_CHOICES = [
         (AC, 'A/C'),
         (NON_AC, 'NON A/C')
     ]
-    number_of_seat = models.IntegerField(max_length=10)
-    class_room_number=models.IntegerField(max_length=5) 
-    floor = models.IntegerField(max_length=5)
-    class_room_type=models.CharField(max_length=10, choices=CLASSROOM_CHOICES, default=NON_AC)
+    number_of_seat = models.IntegerField()
+    class_room_number = models.IntegerField(primary_key=True)
+    floor = models.IntegerField()
+    class_room_type = models.CharField(
+        max_length=10, choices=CLASSROOM_CHOICES, default=NON_AC)
 
     class Meta:
         ordering = ['class_room_number']
