bfe394d37dc0c9c7c03d7a44d7cd70abd3d0eaed
Narendra Kumar <narendra.kumar@rupyz.com>
2024-04-19 18:59:02 +0530

    Created, added serializer

--- classRooms/serializers.py
+++ classRooms/serializers.py
@@ -1,7 +1,9 @@
 from classRooms.models import ClassRoom
-from rest_framework import serializers # type: ignore
+from rest_framework import serializers  # type: ignore
+
 
 class ClassRoomSerializer(serializers.ModelSerializer):
     class Meta:
         model = ClassRoom
-        fields = ['number_of_seat','class_room_number','floor','class_room_type']
+        fields = ['number_of_seat', 'class_room_number',
+                  'floor', 'class_room_type']
