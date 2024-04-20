59444756d7b1a1950bdcd7862cdb8c35f7517f6d
Narendra Kumar <narendra.kumar@rupyz.com>
2024-04-20 19:28:46 +0530

    Modified, to add custom field

--- classRooms/serializers.py
+++ classRooms/serializers.py
@@ -1,3 +1,5 @@
+from student.serializers import StudentSerializer
+from student.models import Student
 from classRooms.models import ClassRoom
 from rest_framework import serializers  # type: ignore
 
@@ -6,4 +8,10 @@ class ClassRoomSerializer(serializers.ModelSerializer):
     class Meta:
         model = ClassRoom
         fields = ['number_of_seat', 'class_room_number',
-                  'floor', 'class_room_type']
+                  'floor', 'class_room_type', 'student']
+    student = serializers.SerializerMethodField()
+
+    def get_student(self, obj):
+        students = Student.objects.filter(class_room=obj)
+        serializer = StudentSerializer(students, many=True)
+        return serializer.data
